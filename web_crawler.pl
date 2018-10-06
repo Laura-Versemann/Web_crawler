68992c8468f3f43b6c5414da9264121be631dbe9
Eike Schwarzwald <dev@eeucalyptus.net>
2018-10-06 23:34:45 +0200

    - Updated regex to use lookaround - Getting url and depth from cli now - Stuff... You know?

--- web_crawler.pl
+++ web_crawler.pl
@@ -1,7 +1,14 @@
 use Modern::Perl;
 use LWP::Simple;
+use strict;
+use Data::Dumper qw(Dumper);
 
-my $start_url = $ARGV[0];
+######### MAIN ###########
+
+say Dumper \@ARGV;
+my $start_url = shift @ARGV;
+my $start_depth = shift @ARGV;
+
 if (not defined $start_url) {
 	say "Enter a valid URL!!!";
 	die;
@@ -9,3 +16,20 @@ if (not defined $start_url) {
 
 say "We are now entering " . $start_url;
 
+get_recursive($start_url, $start_depth);
+
+
+######### FUNCTIONS ###########
+
+sub get_recursive {
+	my ($url, $depth) = @_;
+
+	say "Now getting $url";
+	my $html = get($url);
+	say "got " . substr($html, 0, 20);
+
+	my ($title) = ($html =~ m/<title>(.*)<\/title>/);
+	say $title;	
+	
+	# SAVE THE PAGE!!!!!
+}

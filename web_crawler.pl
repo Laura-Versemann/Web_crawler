f5e77851b256eb781fba66dc40b855110ab8f922
Laura Versemann <laura.versemann@googlemail.com>
2018-10-07 14:36:13 +0200

    zum mergen commitet

--- web_crawler.pl
+++ web_crawler.pl
@@ -1,11 +1,30 @@
 use Modern::Perl;
 use LWP::Simple;
 
-my $start_url = $ARGV[0];
+####################################
+#MAIN
+####################################
+
+my ($start_url, $start_depth) =  @_;
+
 if (not defined $start_url) {
 	say "Enter a valid URL!!!";
 	die;
 }
 
 say "We are now entering " . $start_url;
+
+get_recursive($start_url, $start_depth);
+
+
+
+
+sub get_recursive
+{
+   my ($url, $depth) = @_; 
+   my $html = get($url);
+   my ($title) = ~"<title>.*<\/title>";
+   $title = ~s/ 
+   #Speichern 
+}
 
